ae3b3755d7ca96dc76b9329339a48030bd9f6d7d
subin990218 <122499585+subin990218@users.noreply.github.com>
2024-11-08 15:39:54 +0900

    Title: 알파벳에 따른 평가, Time: 44ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241108/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,16 @@
+alphabet = input()
+
+if alphabet == "S":
+    ans = "Superior"
+elif alphabet == "A":
+    ans = "Excellent"
+elif alphabet == "B":
+    ans = "Good"
+elif alphabet == "C":
+    ans = "Usually"
+elif alphabet == "D":
+    ans = "Effort"
+else:
+    ans = "Failure"
+
+print(ans)
