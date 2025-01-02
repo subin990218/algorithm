d15aeff505771d76231d087dc6f4a44c9d3347de
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-01-02 12:50:57 +0900

    Title: 청소당번 정하기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250102/청소당번 정하기/cleaning-numbering.py
+++ 250102/청소당번 정하기/cleaning-numbering.py
@@ -4,10 +4,8 @@ classroom = 0 # 교실은 2일 마다
 aisle = 0 # 복도는 3일 마다
 bathroom = 0 # 화장실은 12일 마다
 
-for i in range(n):
-    if i == 0:
-        pass
-    elif i % 12 == 0:
+for i in range(1, n+1):
+    if i % 12 == 0:
         bathroom += 1
     elif i % 6 == 0 or i % 3 == 0:
         aisle += 1
