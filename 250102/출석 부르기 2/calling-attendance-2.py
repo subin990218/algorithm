08e500ce2ab2882d52ca2aa484f49ac58fd11f09
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-01-02 16:22:55 +0900

    Title: 출석 부르기 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250102/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+while True:
+    n = int(input())
+
+    if n >= 5:
+        print("Vacancy")
+        break
+    elif n == 1:
+        print("John")
+    elif n == 2:
+        print("Tom")
+    elif n == 3:
+        print("Paul")
+    else:
+        print("Sam")
