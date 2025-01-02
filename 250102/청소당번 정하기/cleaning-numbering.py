b8d1025da5ab14cf247ebdaf20dbc5ba8b748653
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-01-02 12:48:46 +0900

    Title: 청소당번 정하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250102/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+classroom = 0 # 교실은 2일 마다
+aisle = 0 # 복도는 3일 마다
+bathroom = 0 # 화장실은 12일 마다
+
+for i in range(n):
+    if i == 0:
+        pass
+    elif i % 12 == 0:
+        bathroom += 1
+    elif i % 6 == 0 or i % 3 == 0:
+        aisle += 1
+    elif i % 2 == 0:
+        classroom += 1
+
+print(classroom, aisle, bathroom)
+
