abf8c0881a00a983e204fe5eb2952ed65ed468cd
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-01-02 16:06:14 +0900

    Title: 1까지 나누기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250102/1까지 나누기/divide-by-1.py
+++ 250102/1까지 나누기/divide-by-1.py
@@ -1,10 +1,9 @@
 n = int(input())
-ans = n
 
-while ans > 1:
-    for i in range(1, n):
-        if ans // i > 1:
-            ans = ans // i
-        else:
-            print(i)
-            break
+divide = 1
+
+while n > 1:
+    n = n // divide
+    divide += 1
+
+print(divide-1)
