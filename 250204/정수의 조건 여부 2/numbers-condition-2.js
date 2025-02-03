f98628b680a1f9522c659230d20392c8eb12d57a
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-02-04 08:32:28 +0900

    Title: 정수의 조건 여부 2, Time: 182ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250204/정수의 조건 여부 2/numbers-condition-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs")
+
+let input = Number(fs.readFileSync(0).toString().trim())
+
+let ans 
+
+if (input==5) {
+    ans = "A"
+}
+
+if (input %2==0) {
+    ans = "B"
+}
+
+console.log(ans)
