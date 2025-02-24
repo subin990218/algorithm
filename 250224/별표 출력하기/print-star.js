01f02aa76e8bd674afe68e1cc97145cf2fa18f67
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-02-24 11:03:52 +0900

    Title: 별표 출력하기, Time: 195ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250224/별표 출력하기/print-star.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+
+let n = Number(fs.readFileSync(0).toString().trim())
+
+let str = ""
+
+for (let i = 0; i<n; i += 1) {
+    str = ""
+    for (let j = 0; j<=i; j+=1) {
+        str += "* "
+    }
+    console.log(str)
+}
+
+for (let i = n-1; i>0; i-=1) {
+    str = ""
+    for (let j = 0; j<i; j+=1) {
+        str += "* "
+    }
+    console.log(str)
+}
