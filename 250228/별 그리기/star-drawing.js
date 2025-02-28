6d88eefa74871a739cb29632b5b678b8df431f0a
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-02-28 12:45:06 +0900

    Title: 별 그리기, Time: 208ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 250228/별 그리기/star-drawing.js
@@ -0,0 +1,27 @@
+const fs = require("fs")
+
+let n = Number(fs.readFileSync(0).toString().trim())
+
+let str = ""
+
+for (let i = 0; i<n; i+=1) {
+    str = ""
+    for (let j = n-1; j>i; j-=1) {
+        str += " "
+    }
+    for (let j = 0; j<1+2*i; j+=1) {
+        str += "*"
+    }
+    console.log(str)
+}
+
+for (let i = n-2; i>=0; i-=1) {
+    str = ""
+    for (let j = n-1; j>i; j-=1) {
+        str += " "
+    }
+    for (let j = 0; j<1+2*i; j+=1) {
+        str += "*"
+    }
+    console.log(str)
+}
