9f21e44f9e3f8ccfb7c3889779e3d0bb4688c6e7
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-03-03 21:29:04 +0900

    Title: 별표 출력하기 10, Time: 161ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 250303/별표 출력하기 10/print-star-10.js
@@ -0,0 +1,16 @@
+let str = "";
+
+for (let i = 0; i < 6; i++) {
+    str = "";
+        if (i % 2 === 0) {
+                for (let j = 0; j < i + 1; j++) {
+                            str += "*";
+                                    }
+                                        }
+                                            else {
+                                                    str += "$";
+                                                        }
+
+                                                            console.log(str);
+                                                            }
+                                                            
