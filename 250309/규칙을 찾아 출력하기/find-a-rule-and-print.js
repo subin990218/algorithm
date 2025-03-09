47347be95b6f4de64ae366c8fbb6fa20b621bd20
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-03-09 12:32:35 +0900

    Title: 규칙을 찾아 출력하기, Time: 161ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250309/규칙을 찾아 출력하기/find-a-rule-and-print.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+
+let n = Number(fs.readFileSync(0).toString().trim())
+
+let str = ""
+
+for (let i = 0; i < n; i += 1) {
+    str = ""
+    for (let j = 0; j < n; j += 1) {
+        if (i === 0 || i === n-1 || j === 0 || j === n-1) {
+            str += "* " 
+        } else if (i > j) {
+            str += "* "
+        } else {
+            str += "  "
+        }
+    }
+    console.log(str)
+} 
