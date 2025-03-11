db02984afbf528e8d45cd7e5743473fdb587195c
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-03-11 21:55:06 +0900

    Title: 행과 열, Time: 191ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250311/행과 열/row-column.js
+++ 250311/행과 열/row-column.js
@@ -1,11 +1,11 @@
 const fs = require("fs")
-let [a, b] = fs.readFileSync(0).toString().trim().map(Number)
+let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 
 let str = ""
 
-for (let i = 1; i <= n; i += 1 ){
+for (let i = 1; i <= 4; i += 1 ){
     str = ``
-    for (let j = 1; j <= n; j += 1) {
+    for (let j = 1; j <= 5; j += 1) {
         str += `${i*j} ` 
     }
     console.log(str)
