7b2dc878d03df52907dfcb1bde23d67438b75cf4
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-03-12 13:39:47 +0900

    Title: 홀수로 이루어진 정사각형, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

--- 250312/홀수로 이루어진 정사각형/square-with-odd-numbers.js
+++ 250312/홀수로 이루어진 정사각형/square-with-odd-numbers.js
@@ -1,3 +1,13 @@
 const fs = require("fs")
 
 let n = Number(fs.readFileSync(0).toString().trim())
+
+let str = ``
+
+for (let i = 0; i < n; i += 1) {
+    str = ``
+    for (let j = 0; j < n; j += 1) {
+        str += `${11+(i+j)*2} `
+    }
+    console.log(str)
+}
