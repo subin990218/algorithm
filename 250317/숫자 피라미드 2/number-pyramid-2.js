3bec82254a4603ea97193b724272745e995f2e3e
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-03-17 23:39:56 +0900

    Title: 숫자 피라미드 2, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250317/숫자 피라미드 2/number-pyramid-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs")
+
+let n = Number(fs.readFileSync(0).toString().trim())
+
+let str = ``
+let cnt = 1
+
+for (let i = 0; i < n; i += 1) {
+    str = ``
+    for (let j = 0; j <= i; j += 1) {
+        str += `${cnt} `
+        cnt += 1
+    }
+    console.log(str)
+}
