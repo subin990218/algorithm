5c906e47db3dc34595f246865b30bf6663f57db9
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-04-02 17:09:03 +0900

    Title: N개의 숫자 중 최소, Time: 162ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250402/N개의 숫자 중 최소/min-of-n-num.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+
+let input = fs.readFileSync(0).toString().trim().split("\n")
+
+let n = Number(input[0])
+
+let arr = input[1].trim().split(" ").map(Number)
+
+let cnt = 0, minVal = arr[0];
+
+for (let elem of arr.slice(1)) {
+    if (elem < minVal) {
+        minVal = elem
+        cnt = 1
+    } else if (elem === minVal) {
+        cnt += 1
+    }
+}
+
+console.log(minVal, cnt)
