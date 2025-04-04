5884ff627a68348437a450e7b720d3c65caa01b0
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-04-04 19:15:33 +0900

    Title: 두 수의 차의 최솟값, Time: 176ms, Memory: 14MB, Status: Runtime Error - Codetree

---
+++ 250404/두 수의 차의 최솟값/minimum-difference-between-two-numbers.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+
+let input = fs.readFileSync(0).toString().trim().split("\n")
+
+let n = Number(input[0])
+let arr = input[1].trim().split(" ").map(Number)
+
+let ans = 100
+
+for (let i = 0; i < n-1; j += 1) {
+    for (let j = i+1; j < n; j += 1 ) {
+        if (j-i < ans) {
+            ans = j-i
+        }
+    }
+}
+
+
+console.log(ans)
