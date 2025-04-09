d91db491155061371f42afdf7a87feb9e777a4a7
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-04-09 20:36:38 +0900

    Title: 두 개의 격자 비교하기, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

--- 250409/두 개의 격자 비교하기/compare-two-grid.js
+++ 250409/두 개의 격자 비교하기/compare-two-grid.js
@@ -5,30 +5,26 @@ let input = fs.readFileSync(0).toString().trim().split("\n")
 let [n, m] = input[0].split(" ").map(Number)
 
 let arr = []
-let arr2 = []
-
-
 for (let a = 1; a < n+1; a += 1) {
     arr.push(input[a].trim().split(" ").map(Number))
 }
-for (let a = n+1; a < 2*n+1; a += 1) {
-    arr2.push(input[a].trim().split(" ").map(Number))
-}
+
+const arr2 = input.slice(n + 1, 2 * n + 1).map(line => line.split(" ").map(Number));
 
 for (let a = 0; a < n; a += 1) {
     for (let b = 0; b < m; b += 1) {
-        if (arr[a][b] === arr2[a][b]) {
-            arr[a][b] = 0
-        } else {
-            arr[a][b] = 1
-        }
+        arr[a][b] = arr[a][b] === arr2[a][b] ? 0 : 1;
     }
 }
 
 for (let row of arr) {
-    let str = ""
-    for (let elem of row) {
-        str += elem + " "
-    }
-    console.log(str)
+    console.log(row.join(" "))
 }
+
+// 별도 배열 선언해서 푸는 방법
+
+// const result = arr1.map((row, i) =>
+//   row.map((val, j) => (val === arr2[i][j] ? 0 : 1))
+// );
+
+// result.forEach(row => console.log(row.join(" ")));
