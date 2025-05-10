442315cd729dde5ac2f3ac9ce55486b217309a55
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-05-10 18:09:57 +0900

    Title: 최소공배수 구하기, Time: 184ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/최소공배수 구하기/find-the-least-common-multiple.js
+++ 250510/최소공배수 구하기/find-the-least-common-multiple.js
@@ -8,7 +8,7 @@ let [n, m] = input[0].split(' ').map(Number);
 
 function findAns (a,b) {
     let ans = 0
-    for (let i = a; i < a*b; i += 1) {
+    for (let i = a; i <= a*b; i += 1) {
         if (i%a === 0 && i%b === 0) {
             ans = i
             break
