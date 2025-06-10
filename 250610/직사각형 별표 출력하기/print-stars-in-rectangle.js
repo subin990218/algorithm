53cd2809c1a0cd93dcabba3dad9b084d471e508c
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-06-10 22:54:52 +0900

    Title: 직사각형 별표 출력하기, Time: 179ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250610/직사각형 별표 출력하기/print-stars-in-rectangle.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let n = Number(input[0]);
+let m = Number(input[1]);
+
+let str = "";
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < m; j++) {
+        str += "* ";
+    }
+    console.log(str);
+}
