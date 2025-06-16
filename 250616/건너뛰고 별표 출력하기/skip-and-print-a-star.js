b93068fc74e8d53e08bf2291eec8c07992b4e637
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-06-16 23:12:09 +0900

    Title: 건너뛰고 별표 출력하기, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250616/건너뛰고 별표 출력하기/skip-and-print-a-star.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+
+let n = fs.readFileSync(0).toString().trim()
+
+
+for (let i = 0; i < n; i += 1) {
+    for (let j = 0; j <= i; j += 1) {
+        process.stdout.write("*")
+    }
+    console.log()
+    console.log()
+}
+
+for (let i = n-1; i > 0; i -= 1) {
+    for (let j = 0; j < i; j += 1) {
+        process.stdout.write("*")
+    }
+    console.log()
+    console.log()
+}
