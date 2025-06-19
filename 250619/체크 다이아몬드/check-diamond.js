d8889f1b61407f78efdba6462360bf83d509f1c3
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-06-19 16:34:02 +0900

    Title: 체크 다이아몬드, Time: 182ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250619/체크 다이아몬드/check-diamond.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+
+let n = Number(fs.readFileSync(0).toString().trim())
+
+let str = ""
+
+for (let i = 0; i < n; i+=1) {
+    str = ""
+    for (let j = 0; j<n-1-i; j+=1) {
+        str += " "
+    }
+    for (let j = 0; j<i+1; j+=1) {
+        str += "* "
+    }
+    console.log(str)
+}
+for (let i = n-2; i >= 0; i-=1) {
+    str = ""
+    for (let j = 0; j<n-1-i; j+=1) {
+        str += " "
+    }
+    for (let j = 0; j<i+1; j+=1) {
+        str += "* "
+    }
+    console.log(str)
+}
