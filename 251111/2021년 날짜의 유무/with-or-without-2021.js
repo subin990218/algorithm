4990e547edbe002b8d8bf05b1a4ec0d3ebeea7f1
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-11-11 12:22:20 +0900

    Title: 2021년 날짜의 유무, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251111/2021년 날짜의 유무/with-or-without-2021.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let [m, d] = input[0].split(" ").map(Number);
+
+// Please Write your code here.
+
+function maxDay(m) {
+    if (m === 1 || m === 3 || m ===5 || m === 7 || m ===8 || m ===10 || m ===12) {
+        return 31
+    } else if (m === 2) {
+        return 28
+    } else {
+        return 30
+    }
+}
+
+function isValidDate(m, d) {
+    if (maxDay(m)>= d) {
+        return "Yes"
+    } else {
+        return "No"
+    }
+}
+
+console.log(isValidDate(m,d))
