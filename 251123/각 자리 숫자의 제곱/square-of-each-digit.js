e6716d5a073bca5cf667c706c78aa985857d7454
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-11-23 20:59:13 +0900

    Title: 각 자리 숫자의 제곱, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251123/각 자리 숫자의 제곱/square-of-each-digit.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let n = Number(input[0]);
+
+// Please Write your code here.
+
+function plusNumbers(n) {
+    if (n < 10) {
+        return n**2
+    }
+    return plusNumbers(parseInt(n/10)) + (n%10)**2
+}
+
+console.log(plusNumbers(n))
