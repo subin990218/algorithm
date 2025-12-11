c3e89701b5d5cfc26709ba059773b435bd6677e9
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-12-12 00:00:08 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 251211/괄호 쌍 만들어주기 2/pair-parentheses-2.js
+++ 251211/괄호 쌍 만들어주기 2/pair-parentheses-2.js
@@ -2,20 +2,16 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const A = input[0];
 
-let openCount = 0; 
+let openPair = 0;   // 지금까지 등장한 "((" 패턴 개수
 let answer = 0;
 
-for (let i = 0; i < A.length; i++) {
-  if (A[i] === '(') {
-    openCount++;
-  }
+for (let i = 0; i < A.length - 1; i++) {
+  const pair = A[i] + A[i + 1];
 
-  if (i + 1 < A.length) {
-    const pair = A[i] + A[i + 1];
-
-    if (pair === "((" || pair === "))") {
-      answer += openCount;
-    }
+  if (pair === "((") {
+    openPair++;
+  } else if (pair === "))") {
+    answer += openPair;
   }
 }
 
