a5506f1b85113fb04d92148ecafcf3000f395876
subin990218 <122499585+subin990218@users.noreply.github.com>
2025-12-14 22:37:13 +0900

    Title: 씨 오 더블유 2, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251214/씨 오 더블유 2/c-o-w-2.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const string = input[1];
+
+// Please Write your code here.
+let answer = 0;
+
+for (let i = 0; i < n; i++) {
+    if (string[i] !== 'C') continue;
+
+    for (let j = i + 1; j < n; j++) {
+        if (string[j] !== 'O') continue;
+
+        for (let k = j + 1; k < n; k++) {
+            if (string[k] === 'W') {
+                answer++;
+            }
+        }
+    }
+}
+
+console.log(answer);
